a7dcb57b8c18c7671fe43dda8476f3da0651fb5f
AndsoiIo <vladyslav.martonchyk@gmail.com>
2019-11-18 18:36:59 +0200

    version fixed

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="betagr-common",
-    version="0.1.4a",
-    author="AndsoiIo",
-    author_email="andsoiio@gmail.com",
+    version="0.1.5",
+    author="BetAgr Team",
+    author_email="betagr.team@gmail.com",
     description="API-Common package for betagr-project.",
     long_description=long_description,
     long_description_content_type="text/markdown",
