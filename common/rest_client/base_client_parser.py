51e00182362b77b09596e8b0ce7e252947752154
AndsoiIo <vladyslav.martonchyk@gmail.com>
2019-11-19 18:51:55 +0200

    v0.1.6

--- common/rest_client/base_client_parser.py
+++ common/rest_client/base_client_parser.py
@@ -12,22 +12,22 @@ class BaseClientParser(BaseClient):
         super().__init__(headers=headers)
 
     async def get_all_teams(self):
-        url = '/parse-links'
+        url = '/parse-links?parse_by=teams'
         response = await self.get(api_uri=url)
         return response.json
 
     async def get_teams_by_link(self, link_id):
-        url = '/parse-links/{link_id}'.format(link_id=link_id)
+        url = '/parse-links/{link_id}?parse_by=teams'.format(link_id=link_id)
         response = await self.get(api_uri=url)
         return response
 
     async def put_teams_by_link(self, link_id):
-        url = '/parse-links/{link_id}'.format(link_id=link_id)
+        url = '/parse-links/{link_id}?parse_by=teams'.format(link_id=link_id)
         response = await self.put(api_uri=url)
         return response.json
 
     async def delete_teams_by_link(self, link_id):
-        url = '/parse-links/{link_id}'.format(link_id=link_id)
+        url = '/parse-links/{link_id}?parse_by=teams'.format(link_id=link_id)
         response = await self.delete(api_uri=url)
         return response.json
 
