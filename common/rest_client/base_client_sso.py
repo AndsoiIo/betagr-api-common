8052fdd0892aa95938ddd3351a454e820bc7e1b2
AndsoiIo <vladyslav.martonchyk@gmail.com>
2019-11-26 18:20:41 +0200

    permission granted checkout

--- common/rest_client/base_client_sso.py
+++ common/rest_client/base_client_sso.py
@@ -30,13 +30,14 @@ class BaseClientSSO(BaseClient):
         url = '/reset-password'
         return await self.patch(url, body=body)
 
-    async def is_permission_granted(self, cookies: dict):
-        url = '/'
-        if not cookies.get('session'):
+    async def is_permission_granted(self, cookies: dict, permission: str):
+        url = '/is-permission-granted'
+        headers = {'User permission': permission}
+        if cookies.get('session') is None:
             # eliminates a redundant requests for sso server
             logging.warning('The request does not contain a session field in cookies')
             return ClientResponse(status=401, reason="Unauthorized",
                                   headers={'Content-Type': 'application/json'},
                                   json={}, raw_content={})
 
-        return await self.get(api_uri=url, cookies=cookies)
+        return await self.get(api_uri=url, cookies=cookies, headers=headers)
