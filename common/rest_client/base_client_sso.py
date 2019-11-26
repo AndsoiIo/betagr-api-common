bbc2404ac566a8fe1d992afb10e25b10b006644c
AndsoiIo <vladyslav.martonchyk@gmail.com>
2019-11-26 14:13:44 +0200

    added is_permission_granted method to sso [#TODO: add url, handling cookies by BaseClient]

--- common/rest_client/base_client_sso.py
+++ common/rest_client/base_client_sso.py
@@ -1,5 +1,7 @@
 import os
+import logging
 
+from common.utils import client_response as ClientResponse
 from common.rest_client.base_client import BaseClient
 
 
@@ -27,3 +29,14 @@ class BaseClientSSO(BaseClient):
     async def reset_password(self, body):
         url = '/reset-password'
         return await self.patch(url, body=body)
+
+    async def is_permission_granted(self, cookies: dict):
+        url = '/'
+        if not cookies.get('session'):
+            # eliminates a redundant requests for sso server
+            logging.warning('The request does not contain a session field in cookies')
+            return ClientResponse(status=401, reason="Unauthorized",
+                                  headers={'Content-Type': 'application/json'},
+                                  json={}, raw_content={})
+
+        return await self.get(api_uri=url, cookies=cookies)
