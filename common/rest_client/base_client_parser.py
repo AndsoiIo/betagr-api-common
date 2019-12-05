5a2feea0246dcece1d0488cf98c8555586a5949e
tukva <vanton@KH6805.softservecom.com>
2019-12-05 17:41:07 +0200

    Add BaseClientBettingData

--- common/rest_client/base_client_parser.py
+++ common/rest_client/base_client_parser.py
@@ -11,44 +11,8 @@ class BaseClientParser(BaseClient):
     def __init__(self, headers=None):
         super().__init__(headers=headers)
 
-    async def get_all_links(self, params=None):
-        url = '/parse-links'
-        if params:
-            url = f'{url}?parse_by={params}'
-        response = await self.get(api_uri=url)
-        return response
-
-    async def put_all_links(self, params=None):
-        url = '/parse-links'
-        if params:
-            url = f'{url}?parse_by={params}'
-        response = await self.put(api_uri=url)
-        return response
-
-    async def get_by_link(self, link_id, params=None):
-        url = '/parse-links/{link_id}'.format(link_id=link_id)
-        if params:
-            url = f'{url}?parse_by={params}'
-        response = await self.get(api_uri=url)
-        return response
-
-    async def put_by_link(self, link_id, params=None):
-        url = '/parse-links/{link_id}'.format(link_id=link_id)
-        if params:
-            url = f'{url}?parse_by={params}'
-        response = await self.put(api_uri=url)
-        return response
-
-    async def delete_by_link(self, link_id, params=None):
-        url = '/parse-links/{link_id}'.format(link_id=link_id)
-        if params:
-            url = f'{url}?parse_by={params}'
-        response = await self.delete(api_uri=url)
-        return response
-
-    async def change_status_team(self, team_id, json):
-        url = '/change-status-team/{team_id}'.format(team_id=team_id)
-        if not isinstance(json, dict):
-            json = {}
-        response = await self.patch(api_uri=url, data=json)
+    async def parse_team(self, url, cls, elem):
+        parser_url = '/parse'
+        req_json = {"url": url, "cls": cls, "elem": elem}
+        response = await self.post(api_uri=parser_url, data=req_json)
         return response
