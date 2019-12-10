f410124832ba2666bebd05f0d4cade1df7d837ec
tukva <vanton@KH6805.softservecom.com>
2019-12-10 17:48:52 +0200

    Change BaseClientBettingData

--- common/rest_client/base_client_betting_data.py
+++ common/rest_client/base_client_betting_data.py
@@ -8,43 +8,32 @@ class BaseClientBettingData(BaseClient):
     _host = os.getenv('BETTING_DATA_API_HOST')
     _port = int(os.getenv('BETTING_DATA_API_PORT'))
 
-    async def get_all_links(self, params=None):
-        url = '/links'
-        if params:
-            url = f'{url}?data={params}'
+    async def get_teams(self, link_id=None):
+        url = '/teams'
+        if link_id:
+            url = f'{url}?link_id={link_id}'
         response = await self.get(api_uri=url)
         return response
 
-    async def put_all_links(self, params=None):
-        url = '/links'
-        if params:
-            url = f'{url}?data={params}'
+    async def put_teams(self, link_id=None):
+        url = '/teams'
+        if link_id:
+            url = f'{url}?link_id={link_id}'
         response = await self.put(api_uri=url)
         return response
 
-    async def get_by_link(self, link_id, params=None):
-        url = '/links/{link_id}'.format(link_id=link_id)
-        if params:
-            url = f'{url}?data={params}'
+    async def get_real_teams(self):
+        url = '/real-teams'
         response = await self.get(api_uri=url)
         return response
 
-    async def put_by_link(self, link_id, params=None):
-        url = '/links/{link_id}'.format(link_id=link_id)
-        if params:
-            url = f'{url}?data={params}'
+    async def put_real_teams(self):
+        url = '/real-teams'
         response = await self.put(api_uri=url)
-        return response
-
-    async def delete_by_link(self, link_id, params=None):
-        url = '/links/{link_id}'.format(link_id=link_id)
-        if params:
-            url = f'{url}?data={params}'
-        response = await self.delete(api_uri=url)
         return response
 
     async def change_status_team(self, team_id, json):
-        url = '/change-status-team/{team_id}'.format(team_id=team_id)
+        url = '/team/{team_id}'.format(team_id=team_id)
         if not isinstance(json, dict):
             json = {}
         response = await self.patch(api_uri=url, data=json)
